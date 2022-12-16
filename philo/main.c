59ffdff990860a1c9bb8292058b45d10a9b6e2a0
znichola <znichola@student.42lausanne.ch>
2022-12-16 12:11:19 +0100

    starting with pthreads

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 20:54:51 by znichola          #+#    #+#             */
-/*   Updated: 2022/12/15 22:44:24 by znichola         ###   ########.fr       */
+/*   Updated: 2022/12/16 12:10:27 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,65 @@
 // usleep, gettimeofday, pthread_create, pthread_detach, pthread_join,
 // pthread_mutex_init, pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
 
+// number_of_philosophers time_to_die time_to_eat time_to_sleep
+// [number_of_times_each_philosopher_must_eat]
+
+int	x = 0;
+pthread_mutex_t	mutex;
+
+void	*routine()
+{
+	printf("test of a new thread pid%d\n", getpid());
+	for (int i = 0; i < 1000000; i++)
+	{
+		pthread_mutex_lock(&mutex);
+		x += 1;
+		pthread_mutex_unlock(&mutex);
+	}
+	sleep(3);
+	printf("ending thread x:%d\n", x);
+	return (NULL);
+}
+
 int	main(int ac, char **av)
 {
 	t_app a;
 	
-	return (0);
+	if (ac > 6 || ac < 5)
+		return (1);
+	if(ft_safe_atoi(&a.philo_count, av[1]) + ft_safe_atoi(&a.ttdie, av[2]) 
+		+ ft_safe_atoi(&a.tteat, av[3]) + ft_safe_atoi(&a.ttsleep, av[4]))
+		return (1);
+	if (ac == 6)
+		if (ft_safe_atoi(&a.meals, av[5]))
+			return (1);
+	
+	pthread_t	t[a.philo_count];
+
+	if (pthread_mutex_init(&mutex, NULL))
+		return (1);
+
+	print_app(&a);
+	int	i;
+
+	i = -1;
+	while (++i < a.philo_count)
+	{
+		if (pthread_create(t + i, NULL, &routine, NULL))
+			return (1);
+		printf("thread:%d started\n", i);
+	}
+	i = -1;
+	while (++i < a.philo_count)
+	{
+		if (pthread_join(t[i], NULL))
+			return (2);
+		printf("thread:%d has finished\n", i);
+	}
+	
+	if (pthread_mutex_destroy(&mutex))
+		return (1);
+	
+	printf("final x:%d", x);
+	return (SUCCESS);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 20:55:22 by znichola          #+#    #+#             */
-/*   Updated: 2022/12/16 00:12:06 by znichola         ###   ########.fr       */
+/*   Updated: 2022/12/16 03:04:42 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,55 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+# define SUCCESS 0
+# define FAILURE -1
+
+# define FT_INT_MAX 2147483647
+# define FT_INT_MIN -2147483648
+
+typedef enum e_bool
+{
+	false = 0,
+	true = 1
+}	t_bool;
 
 typedef struct s_philo
 {
-	
+	int		number;
+	int		diet;
+	int		eatt;
+	int		sleept;
+	int		meals;
+	int		is_eating;
+	int		my_fork; // should be a mutex
+	t_bool	dead;
+	struct s_philo *to_left;
 }	t_philo;
 
 typedef struct s_app
 {
-	int	philo_num;
+	int	philo_count;
 	int	ttdie;
 	int	tteat;
 	int	ttsleep;
-	int	num_meals;
+	int	meals;
 }	t_app;
+
+
+/* safe atoi */
+
+int		safe_add(int *a, int b);
+int		safe_multi(int *a, int b);
+int		ft_safe_atoi(int *n, char *str);
+
+/* debug */
+
+void	print_philo(t_philo *p);
+void	print_app(t_app *a);
+void	print_log(t_app *a);
+
+
 
 #endif /* philo */
